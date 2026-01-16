583e1b0c3d2ac30de40a3833dc507c348966cb7e
Jeremy Villa <villajeremy1991@gmail.com>
2026-01-16 11:32:02 -0500

    Made it so events can be deleted, regardless if they have RSVPs (#10)

--- EventRsvp.Domain/Interfaces/IRsvpRepository.cs
+++ EventRsvp.Domain/Interfaces/IRsvpRepository.cs
@@ -7,4 +7,5 @@ public interface IRsvpRepository
     Task<Rsvp> AddAsync(Rsvp rsvp, CancellationToken cancellationToken = default);
     Task<IEnumerable<Rsvp>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<IEnumerable<Rsvp>> GetByEventIdAsync(int eventId, CancellationToken cancellationToken = default);
+    Task DeleteByEventIdAsync(int eventId, CancellationToken cancellationToken = default);
 }
